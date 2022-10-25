599bb806b6c2d1c664f20613ceb617371730013a
abraham <apolichenco@gmail.com>
2022-10-24 22:45:28 -0400

    commit tree af074a7d6eb07687e7a72730d88bd910f8e3ff85

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :left, :right, :bottom
+
+  def initialize(left, right, bottom)
+    @left = left
+    @right = right
+    @bottom = bottom
+  end
+
+  def kind
+    # binding.pry
+    if left <= 0 || right <= 0 || bottom <= 0
+      raise TriangleError
+    elsif (left + right) <= bottom || (left + bottom) <= right || (right + bottom) <= left
+      raise TriangleError
+    elsif left == right && right == bottom      
+      :equilateral
+    elsif left == right || right == bottom || bottom == left
+      :isosceles
+    else
+      :scalene
+    end
+  end
+      
+  class TriangleError < StandardError
+    "Not a valid triangle"
+  end
+
 end
